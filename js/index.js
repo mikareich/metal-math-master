72637afda16ad35f28881aaeb4f087fb05d14cd7
Mika Reich <mikareich0@gmail.com>
2022-05-06 23:43:00 +0200

    connect to exercises.json

--- js/index.js
+++ js/index.js
@@ -1,33 +1,34 @@
-const exercisesList = document.getElementById("exercisesList");
+import formatOperators from "./formatOperators.js";
+import loadExercises from "./getExercises.js";
 
-// load exercises
-async function loadExercises() {
-  try {
-    const response = await fetch("/assets/exercises.json");
-    const exercises = await response.json();
-
-    return exercises;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
-}
-
+// render exercises
 function createExercise(exercise) {
   const li = document.createElement("li");
+
   li.innerHTML = `
-    <li class="exercise-container">
-        <a class="exercise__link" href="/exercise.html?id=${exercise.id}">
-            <span class="exercise__index">${exercise.id + 1}</span>
-            <span class="exercise__operators">a+b</span>
-            <span class="exercise__difficulty exercise__difficulty-easy">${
-              exercise.difficulty
-            }</span>
-        </a>
-    </li>
-  `;
+      <li class="exercise-container">
+          <a class="exercise__link" href="/exercise.html?id=${exercise.id}">
+              <span class="exercise__index">${exercise.id + 1}</span>
+              <span class="exercise__operators">${formatOperators(
+                exercise.operators.join(", ")
+              )}</span>
+              <span class="exercise__difficulty exercise__difficulty-${
+                exercise.difficulty
+              }">${exercise.difficulty}</span>
+          </a>
+      </li>
+    `;
+
   return li;
 }
 
-/** Formats * and / into  */
-function formatOperators(operators) {}
+const exercisesList = document.getElementById("exercisesList");
+
+window.addEventListener("load", async () => {
+  const exercises = await loadExercises();
+
+  exercises.forEach((exercise) => {
+    const li = createExercise(exercise);
+    exercisesList.appendChild(li);
+  });
+});
